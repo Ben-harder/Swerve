1dfd8e9f1e29335e37d0cf587b9286c5340a3a66
Jeremy H. (33860157) <jeremyh1101@gmail.com>
2018-09-16 07:27:20 -0700

    Added picture change

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight, Alert, Linking } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableOpacity, Alert, Linking } from 'react-native';
+import FormData from 'FormData';
+
+const a = require('./img/fritz.jpg');
+const b = require('./img/icon.jpg');
 
 export default class App extends React.Component
 {
@@ -9,7 +13,42 @@ export default class App extends React.Component
     this.state = { count: 0};
     this.index = 0;
     this.date = { date: (new Date()).toString() }
-    this.url_array = ['https://www.youtube.com/watch?v=fKopy74weus', 'https://reactnativecode.com', 'https://www.youtube.com/watch?v=1KAE_JJx0-I', 'http://puppy-picturee.blogspot.com/2012/10/siberian-husky-puppy-pictures.html', 'https://3.bp.blogspot.com/-8sZkKWu98rE/UiD3w_ilIuI/AAAAAAAAAUc/ywb03PKeRtA/s1600/cat-funny.jpg'];
+    this.url_array = ['https://www.youtube.com/watch?v=fKopy74weus', 'https://reactnativecode.com', 'https://www.youtube.com/watch?v=1KAE_JJx0-I', 'http://puppy-picturee.blogspot.com/2012/10/siberian-husky-puppy-pictures.html',
+    'https://3.bp.blogspot.com/-8sZkKWu98rE/UiD3w_ilIuI/AAAAAAAAAUc/ywb03PKeRtA/s1600/cat-funny.jpg', 'https://i.imgur.com/k9i7YLN.jpg'];
+
+    this.timer = null;
+    this.addOne = this.addOne.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.number = 0;
+
+    this.view = true;
+    this.view_url = require('./img/icon.jpg');
+
+  }
+  
+  addOne() {
+    this.number = this.number + 1;
+    this.timer = setTimeout(this.addOne, 200);
+  }
+
+  stopTimer() {
+    var number = this.number;
+    this.number = 0;
+    clearTimeout(this.timer);
+
+    if(number >= 3){
+      console.log(number);
+      this.view = true;
+      this.stayCount();
+    }
+    else{
+      this.view = false;
+      this.post_request(this.getDate())
+      this.incrementCount();
+      getURL(this.url_array[this.updateIndex(0, 6)]);
+    }
+    this.view_url = this.view === true ? a : b;
+    return;
   }
 
   random_range(lower_limit, upper_limit)
@@ -27,6 +66,10 @@ export default class App extends React.Component
     return this.index
   }
 
+  stayCount(){
+    this.setState(prevState => ({ count: prevState.count}))
+  }
+
   incrementCount()
   {
     this.setState(prevState => ({ count: prevState.count + 1 }))
@@ -37,26 +80,52 @@ export default class App extends React.Component
     return (new Date()).toString();
   }
 
-  render()
-  {
-    return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={() =>
-        {
-          this.incrementCount();
-          getURL(this.url_array[this.updateIndex(0, 5)]);
-        }}>
+  async post_request(item){
+    var formData = new FormData();
+    formData.append('date', item);
+    console.log(item);
+    try{
+      let req = await fetch('http://ben-harder.com/lumohacks2019/execPython.php',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        body: formData,
+      });
+      if (req.status >= 200 && req.status < 300) {
+        console.log(req.status);
+      }
+      else{
+        console.log(req.status);
+      }
+    }
+    catch(errors){
+      console.log(errors);
+    }
+  }
 
-          <Image
-            source={require('./img/icon.jpg')}
-          />
-        </TouchableHighlight>
-        <Text style={{color: 'white'}}>{this.getDate()}</Text>
-      </View>
+  display_image(){
+    console.log(this.view_url);
+    return (
+      <Image
+        source={this.view_url}
+      />
     );
   }
-}
 
+  render()
+  {
+      return (
+
+        <View style={styles.container}>
+          <TouchableOpacity onPressIn={this.addOne} onPressOut={this.stopTimer} >
+          {this.display_image()}
+          </TouchableOpacity>
+          <Text style={{color: 'white'}}>{this.getDate()}</Text>
+        </View>
+      );
+  }
+}
 function getURL(url){
     Linking.canOpenURL(url).then(supported => {
       if (!supported) {
