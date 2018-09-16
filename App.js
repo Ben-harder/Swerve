253df2d67d1092d42e2814c1508bc8da4608482f
Ben <ben.harder2@gmail.com>
2018-09-15 17:52:30 -0700

    date working

--- App.js
+++ App.js
@@ -3,18 +3,37 @@ import { StyleSheet, Text, View, Image, TouchableHighlight, Alert } from 'react-
 
 export default class App extends React.Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.state = { count: 0 };
+    this.date = { date: (new Date()).toString() }
+  }
+
+  incrementCount()
+  {
+    this.setState(prevState => ({ count: prevState.count + 1 }))
+  }
+
+  getDate()
+  {
+    return (new Date()).toString();
+  }
+
   render()
   {
     return (
       <View style={styles.container}>
         <TouchableHighlight onPress={() =>
         {
-          Alert.alert("wain");
+          this.incrementCount();
         }}>
+
           <Image
             source={require('./img/icon.jpg')}
           />
         </TouchableHighlight>
+        <Text>{this.getDate()}</Text>
       </View>
     );
   }
